5e70122b3550c6159ef05f81b73dda827b8cc3d2
Elias Ortiz <macpc@MacBook-Air-de-MACPC.local>
2026-02-01 21:28:59 -0300

    feat:(AJFS-Fundamentos) add operaciones de incremento y decremento

---
+++ AJFS-Fundamentos/Operaciones-Numeros/OperacionesIncrem_decrem.js
@@ -0,0 +1,9 @@
+
+// Operaciones Numericas de incremento y decremento
+
+let variable = 7; 
+console.log("Valor:", variable)
+variable ++;
+console.log("Valor despues del incremento:", variable)
+variable --;
+console.log("Valor despues del decremento :", variable)
