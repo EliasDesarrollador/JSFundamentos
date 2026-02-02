fd08e71a7af5544f76c10e0f29f9dd91523f9a2e
Elias Ortiz <macpc@MacBook-Air-de-MACPC.local>
2026-02-01 21:42:39 -0300

    fix:(AJFS-Fundamentos) correcion de OperacionesComplejas

--- AJFS-Fundamentos/Operaciones-Numeros/OperacionesComplejas.js
+++ AJFS-Fundamentos/Operaciones-Numeros/OperacionesComplejas.js
@@ -13,6 +13,7 @@ let resultado1 =  (numero1 * numero2) +  numero3  / numero4 ;
 let resultado2 = numero1*  (numero2 + numero3 / numero4 );
 let resultado3 = numero1 * (numero2 + numero3 ) / numero4; 
 
+console.log("Operaciones Numericas Complejas");
 console.log('Operacion: (%s * %s)  + %s  / %s  = %s ', numero1, numero2, numero3, numero4, resultado1);
-console.log('Operacion: %s * (%s  + %s / %s) ' = numero1, numero2, numero3, numero4, resultado2);
-console.log('Operacion: %s * (%s  + %s )  / %s ' = numero1, numero2, numero3, numero4 , resultado3);
+console.log('Operacion: %s * (%s  + %s / %s) '  ,numero1, numero2, numero3, numero4, resultado2);
+console.log('Operacion: %s * (%s  + %s )  / %s ' , numero1, numero2, numero3, numero4 , resultado3);
