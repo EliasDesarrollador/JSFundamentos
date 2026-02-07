ff8d6551e20a871149f15f602d04cd33bdbddf54
esam-dev <samuelortizamarilla@gmail.com>
2026-02-07 18:48:56 -0300

    feat: agregado de ejercicios de operacion compuesta

--- AJFS-Fundamentos/Operaciones-Numeros/OperadoresAsigCompuestas.js
+++ AJFS-Fundamentos/Operaciones-Numeros/OperadoresAsigCompuestas.js
@@ -1,5 +1,8 @@
 
 // Ejercicio de operadores de asignacion compuesta 
+console.log("Operadores de Asignacion Compuesta");
+
+console.log("Valores iniciales: numero1 = 8, numero2 = 3");
 
 let numero1 = 8;
 let numero2 = 3; 
@@ -11,3 +14,9 @@ console.log("numero1 -= numero2 :", numero1);
 numero1 = 8; 
 numero1 *= numero2; 
 console.log(" numero1 *= numero2 :", numero1);
+numero1 /= numero2;
+console.log(" numero1 /= numero2 :", numero1);
+numero1 %= numero2; 
+console.log(" numero1 %= numero2 :", numero1);
+numero1 **= numero2;
+console.log(" numero1 **= numero2 :", numero1);
