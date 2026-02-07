08ba1a1a274a5d4759ed20dd8e217f2b313a676f
esam-dev <samuelortizamarilla@gmail.com>
2026-02-07 19:20:47 -0300

    feat:agregado de ejercicio de operaciones con decimales

---
+++ AJFS-Fundamentos/Operaciones-Numeros/operacionesDecimales.js
@@ -0,0 +1,21 @@
+
+// Fase 4 - Operaciones Aritmeticas con numeros decimales 
+console.log("Operaciones Aritmeticas con Numeros Decimales");
+
+let numero1 = 9.34;
+let numero2 = 3.1; 
+let resultadoSuma = numero1 + numero2;
+let resultadoResta = numero1 - numero2;
+let resultadoMultiplicacion = numero1 * numero2; 
+let resultadoDivision = numero1 * numero2;
+let resultadoModulo = numero1 % numero2;
+let resultadoExponente = numero1 ** numero2;
+console.log("Numero 1: ", numero1);
+console.log("Numero 2: ", numero2);
+console.log("Resultado de la Suma:",resultadoSuma);
+console.log("Resultado de la Resta:", resultadoResta);
+console.log("Resulatado de la Multiplicacion:", resultadoMultiplicacion);
+console.log("Resultado de la Division:", resultadoDivision);
+console.log("Resultado del Modulo:", resultadoModulo);
+console.log("Resultado del Exponente:", resultadoExponente);
+console.log("---Fin de Proceso---");
